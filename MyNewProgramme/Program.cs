80938d6771ebaa79afa63c252c43eed6c1e986d4
eallenOP <eallen@op.ac.nz>
2018-09-19 14:09:48 +1200

    Stub out a programme skeleton

--- MyNewProgramme/Program.cs
+++ MyNewProgramme/Program.cs
@@ -10,6 +10,20 @@ namespace MyNewProgramme
     {
         static void Main(string[] args)
         {
+            //Hello world goes here
+            Console.Write();
+
+            //If statement here
+            if (true)
+            {
+
+            }
+
+            //Put a for loop here...
+            for (int i = 0; i < length; i++)
+            {
+
+            }
         }
     }
 }
